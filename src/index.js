b3f676b79de3f1321dc22c1a72c3dbd71aeda845
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-08-09 09:30:44 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const list = document.querySelector("ul");
+
+  document.getElementById("create-task-form").addEventListener("submit", function(event){
+    event.preventDefault()
+    const userInput = event.target.new_task_description.value;
+    const newTask = document.createElement("li");
+    newTask.textContent = userInput;
+    list.appendChild(newTask);
+
+    const deleteButton = document.createElement("button");
+    newTask.appendChild(deleteButton);
+    deleteButton.addEventListener("click", function(event){
+      list.removeChild(newTask)});
+
+    console.log(event)
+  });
+
+
 });
